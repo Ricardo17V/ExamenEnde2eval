2e7dbb94f53db5314555a3ac59e3502e78d3d848
ricardo <ricardo@DESKTOP-KLL37QO>
2026-03-06 14:00:07 +0100

    Documentación de la clase CuentaBancaria

--- ExamenEnde2eval/src/exa2ende2526/enunciado/CuentaBancaria.java
+++ ExamenEnde2eval/src/exa2ende2526/enunciado/CuentaBancaria.java
@@ -1,11 +1,27 @@
 package exa2ende2526.enunciado;
+/**
+ * @author ricardo
+ * @date 06/03/2026
+ */
 
+/**
+ * Clase padre CuentaBancaria de las que sus hijas heredaran sus atributos y su método abstracto calcularIntereses,
+ * posee también los métodos ingresar, reintegrar
+ */
 public abstract class CuentaBancaria {
-
+	/**
+	 * @param titular es el nombre del titular de la cuenta bancaria
+	 */
     protected String titular;
+    /**
+     * @param saldo es el saldo que posee la cuenta bancaria
+     */
     protected double saldo;
-    
-
+    /**
+     * Constructor con los atributos titular y saldoInicial que en caso de tener saldo inicial negativo lanza una excepción
+     * @param titular es el nombre del titular de la cuenta bancaria 
+     * @param saldoInicial es el saldo que inicialmente posee una cuenta bancaria
+     */
     public CuentaBancaria(String titular, double saldoInicial) {
         if (saldoInicial < 0) {
             throw new IllegalArgumentException("El saldo inicial no puede ser negativo");
@@ -13,30 +29,43 @@ public abstract class CuentaBancaria {
         this.titular = titular;
         this.saldo = saldoInicial;
     }
-
+    /**
+     * Método que calcula los intereses de una cuenta bancaria
+     * @param saldo es el saldo que posee la cuenta bancaria
+     * @return devuelve el interés calculado
+     * @throws lanza una excepción, aún no implementada
+     */
+    public abstract double calcularInteres(double saldo) throws Exception;
+    /**
+     * Método para obtener el nombre del titular de la cuenta
+     * @return devuelve el nombre del titular
+     */
     public String getTitular() {
         return titular;
     }
-
+    /**
+     * Método para obtener el salario de la cuenta
+     * @return devuelve el valor del salario de la cuenta
+     */
     public double getSaldo() {
         return saldo;
     }
-
-    public void ingresar(double cantidad) throws Exception{
+    /**
+     * Método para ingresar cierta cantidad a la cuenta
+     * @param cantidad
+     */
+    public void ingresar(double cantidad) {
 
         saldo += cantidad;
-        
-        if (cantidad > 0) {
-			throw new Exception("La cantidad no puede ser negativa");
-		}
     }
-
-    public void reintegrar(double cantidad) throws  Exception{
+    /**
+     * Método para reintegrar cierta cantidad de la cuenta
+     * @param cantidad
+     */
+    public void reintegrar(double cantidad) {
 
         saldo -= cantidad;
-        if (cantidad > 0 && saldo>cantidad) {
-			throw new Exception("La cantidad no puede ser negativa ni superior al saldo");
-		}
     }
-
+    
+    
 }
